aab705a1856bdf98b73edc53a3967425d98913dd
Gen4ralz <76762117+Gen4ralz@users.noreply.github.com>
2023-02-09 21:29:13 +0700

    Add Server-Side form validation

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"github.com/gen4ralz/booking-app/internal/config"
+	"github.com/gen4ralz/booking-app/internal/forms"
 	"github.com/gen4ralz/booking-app/internal/models"
 	"github.com/gen4ralz/booking-app/internal/render"
 )
@@ -53,7 +54,45 @@ func (m *Repository) Majors (res http.ResponseWriter,req *http.Request) {
 }
 
 func (m *Repository) Reservation (res http.ResponseWriter,req *http.Request) {
-	render.RenderTemplate(res,req, "reservation.gohtml", &models.TemplateData{})
+	var emptyReservation models.Reservation
+	data := make(map[string]interface{})
+	data["reservation"] = emptyReservation
+
+	render.RenderTemplate(res,req, "reservation.gohtml", &models.TemplateData{
+		Form: forms.New(nil),
+	})
+}
+
+func (m *Repository) PostReservation (res http.ResponseWriter,req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	reservation := models.Reservation{
+		FirstName: req.FormValue("first_name"),
+		LastName: req.FormValue("last_name"),
+		Email: req.FormValue("email"),
+		Phone: req.FormValue("phone"),
+	}
+
+	form := forms.New(req.PostForm)
+
+	// form.Has("first_name", req)
+	form.Required("first_name", "last_name", "email", "phone")
+	form.MinLength("first_name", 3, req)
+	form.IsEmail("email")
+
+	if !form.Valid() {
+		data := make(map[string]interface{})
+		data["reservation"] = reservation
+
+		render.RenderTemplate(res,req, "reservation.gohtml", &models.TemplateData{
+			Form: form,
+			Data: data,
+		})
+	}
 }
 
 func (m *Repository) Availability (res http.ResponseWriter,req *http.Request) {
